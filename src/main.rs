2e251e3c581d9d2c7b9ce382fbdef226dd8426c1
Mykyta Sikachyna <sikachyna.n@gmail.com>
2025-02-12 19:34:00 +0200

    feat: create tray icon

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,62 @@
+#![windows_subsystem = "windows"]
+
+use tao::{
+    event::Event,
+    event_loop::{
+        ControlFlow,
+        EventLoopBuilder
+    }
+};
+use tray_icon::{
+    menu::{
+        Menu, MenuEvent, MenuItem
+    },
+    TrayIcon, TrayIconBuilder
+};
+
+mod helpers;
+
+enum UserEvent {
+    MenuEvent(MenuEvent)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let light_icon_path = concat!(env!("CARGO_MANIFEST_DIR"), "/assets/light_icon.png");
+    let light_icon = helpers::load_icon(std::path::Path::new(light_icon_path));
+
+    let event_loop = EventLoopBuilder::<UserEvent>::with_user_event().build();
+    let proxy = event_loop.create_proxy();
+    MenuEvent::set_event_handler(Some(move |event| {
+        let _ = proxy.send_event(UserEvent::MenuEvent(event));
+    }));
+
+    let tray_menu = Menu::new();
+    let quit_item = MenuItem::new("Quit", true, None);
+    let _ = tray_menu.append_items(&[
+        &quit_item
+    ]);
+
+    let mut tray_icon: Option<TrayIcon> = None;
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+
+        match event {
+            Event::NewEvents(tao::event::StartCause::Init) => {
+                tray_icon = Some(
+                    TrayIconBuilder::new()
+                        .with_menu(Box::new(tray_menu.clone()))
+                        .with_icon(light_icon.clone())
+                        .build()
+                        .unwrap()
+                );
+            }
+            Event::UserEvent(UserEvent::MenuEvent(event)) => {
+                if event.id == quit_item.id() {
+                    tray_icon.take();
+                    *control_flow = ControlFlow::Exit;
+                }
+            }
+            _ => {}
+        }
+    });
 }
